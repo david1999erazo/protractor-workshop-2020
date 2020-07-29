9e1c8fed2a7bee4d60c5ba8dba1f1f00c960767d
David Erazo <33490042+david1999erazo@users.noreply.github.com>
2020-07-28 20:40:08 -0500

    Add static code analysis (#7)

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,7 +3,7 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/google.spec.js' ],
+  specs: ['../test/google.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
@@ -16,6 +16,3 @@ export const config: Config = {
     }
   }
 };
-
-
-
